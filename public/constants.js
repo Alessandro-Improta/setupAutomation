23e402747f6c48143ee027352ba3a77c331cd036
Alessandro Improta <aimprota@Alessandros-MacBook-Air.local>
2017-12-19 03:14:51 -0700

    added ecosystem.json

--- public/constants.js
+++ public/constants.js
@@ -1,6 +1,6 @@
 angular.module('setupApp').service('constants', function($http){
 
-	let appUrl = 'http://192.168.0.100:3000';
+	let appUrl = 'http://fillyourseats.zapto.org:3000';
 
 	this.appUrl = appUrl;
 
