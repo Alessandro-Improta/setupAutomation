62016820c2d92a22685659e3d7f2c7810422a44f
Alessandro Improta <aimprota30@gmail.com>
2017-12-19 09:56:01 -0700

    fixes for server

--- public/main/mainController.js
+++ public/main/mainController.js
@@ -66,17 +66,10 @@ angular.module('setupApp').controller('mainController', function($scope, $locati
 	};
 
 	let addData = function(keys, values) {
-		return $http({
-			method: 'PUT',
-			url: appUrl + '/addData',
-			data: {
-				keys: keys,
-				values: values
-			}
-		})
-		.then(function(res) {
-			console.log(res.data.message);
-		})
+		for(var i = 0; i < keys.length; i++) {
+			sessionStorage.setItem(keys[i], values[i]);
+		}
+		console.log(sessionStorage);
 	};
 
 	let getTemplate = function() {
