33060bb6a6f659507897998efcc9e1c9b420c45c
Alessandro Improta <aimprota30@gmail.com>
2018-01-13 03:24:30 -0700

    trying83

--- auth.js
+++ auth.js
@@ -6,14 +6,7 @@ const axios = require('axios');
 
 
  
-let adwordsUser = new AdwordsUser({
-    developerToken: 'NRfUDgxy825XbJ-jmNbLZQ', //your adwords developerToken
-    userAgent: 'Fill Your Seats', //any company name
-    // clientCustomerId: the Adwords Account id (e.g. 123-123-123)
-    client_id: '1037770292-oohlht2dnieanagkcmt90o8979grn3h8.apps.googleusercontent.com', //this is the api console client_id
-    client_secret: 'D1ht5Wso2vydo5XIKD4_fO3G',
-    // refresh_token: 'INSERT_OAUTH2_REFRESH_TOKEN_HERE'
-});
+let adwordsUser;
 
 let oauth2Client = new OAuth2(
   '1037770292-oohlht2dnieanagkcmt90o8979grn3h8.apps.googleusercontent.com',
@@ -90,6 +83,14 @@ module.exports = {
     			oauth2Client.setCredentials(tokens);
     			google.options({
   					auth: oauth2Client
+				});
+				adwordsUser = new AdwordsUser({
+				    developerToken: 'NRfUDgxy825XbJ-jmNbLZQ', //your adwords developerToken
+				    userAgent: 'Fill Your Seats', //any company name
+				    // clientCustomerId: the Adwords Account id (e.g. 123-123-123)
+				    client_id: '1037770292-oohlht2dnieanagkcmt90o8979grn3h8.apps.googleusercontent.com', //this is the api console client_id
+				    client_secret: 'D1ht5Wso2vydo5XIKD4_fO3G',
+				    // refresh_token: 'INSERT_OAUTH2_REFRESH_TOKEN_HERE'
 				});
 				if (req.body.customerId) {
 					let customerId = req.body.customerId;
@@ -178,7 +179,7 @@ module.exports = {
 				managerCustomerId: '7908685882',
 				clientCustomerId: req.body.customerId,
 				linkStatus: 'ACTIVE',
-				pendingDescriptiveName: 'Sent from FYS MCC',
+				pendingDescriptiveName: 'Accept link',
 				isHidden: 'false'
 			}
 		}];
